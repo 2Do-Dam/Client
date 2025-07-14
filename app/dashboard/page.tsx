f7840ecbf3145c4f2f9b5bc60d4fe862f35e6f09
김우찬 <dnckss1121@gmail.com>
2025-07-14 20:00:43 +0900

    add :: 그래프 추가

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import React from 'react';
 import Calendar from '@/features/calendar/components/Calendar';
+import Graph from '@/features/graph/Graph';
 
 export default function Dashboard() {
   return (
     <div>
       <Calendar />
+      <Graph />
     </div>
   );
 }
